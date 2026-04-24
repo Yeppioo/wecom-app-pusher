549e1349c34b1cba85914911ce230808f5f17d9f
tiouoo <tiouo@qq.com>
2026-04-24 17:34:20 +0800

    fix(package): 更新作者信息

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ const agentid = "1000001";
 const pusher = new WeComPusher(corpid, corpsecret, agentid);
 
 (async () => {
-  const touser = "yiiko";
+  const touser = "tiouo";
   const title = "早上好提醒！";
   const description = `今天天气晴朗，适合外出。`;
   const redirectUrl = "https://example.com/more-info";
